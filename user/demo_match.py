07c7cd36eaafe7777c2b02cff3daf3fe9949b063
Ambrose Ling <aling@tenstorrent.com>
2025-10-25 01:05:14 -0400

    add docs

--- user/demo_match.py
+++ user/demo_match.py
@@ -1,18 +1,22 @@
 from environment.environment import RenderMode, CameraResolution
-from environment.agent import run_real_time_match
+from environment.agent import run_match
 from user.train_agent import UserInputAgent, BasedAgent, ConstantAgent, ClockworkAgent, SB3Agent, RecurrentPPOAgent #add anymore custom Agents (from train_agent.py) here as needed
 from user.my_agent import SubmittedAgent
+import pygame
+pygame.init()
 
 my_agent = UserInputAgent()
+
 #Input your file path here in SubmittedAgent if you are loading a model:
-opponent = SubmittedAgent(file_path="checkpoints/experiment_6/rl_model_78576_steps")
+opponent = SubmittedAgent()
 
 match_time = 99999
 
 # Run a single real-time match
-run_real_time_match(
+run_match(
     agent_1=my_agent,
     agent_2=opponent,
     max_timesteps=30 * match_time,  # Match time in frames (adjust as needed)
     resolution=CameraResolution.LOW,
+    video_path='tt_agent.mp4'
 )
